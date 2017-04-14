fa7f4a9a5eff2b001f37b513c3d823bf2fc598af
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-04-14 17:30:38 -0300

    Criado a ControllerServlet, a ControllerFuncionario e interface Logica.

---
+++ perfumaria/src/main/java/br/senac/tads/pi3a/controller/ControllerServlet.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Elton.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.senac.tads.pi3a.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Elton
+ */
+@WebServlet("/perfumaria")
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(
+            HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String parametro = request.getParameter("controller");
+        String nomeClasse = "br.senac.tads.pi3a.controller.Controller" + parametro;
+        
+        try {
+            Class<?> classe = Class.forName(nomeClasse);
+            Logica logica = (Logica) classe.newInstance();
+            
+            // Recebe a String após a execução da lógica
+            String pagina = logica.executa(request, response);
+            
+            // Faz o forwand para a página JSP
+            request.getRequestDispatcher(pagina).forward(request, response);
+        } catch (
+                ClassNotFoundException | 
+                InstantiationException | 
+                IllegalAccessException | 
+                ServletException | 
+                IOException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
