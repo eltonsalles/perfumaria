4e7f5d81223da2d5a0f891aaf6be2d52b0073011
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-04-16 15:46:34 -0300

    Criado o pacote Model e a classe Fncionario.

---
+++ perfumaria/src/main/java/br/senac/tads/pi3a/model/Funcionario.java
@@ -0,0 +1,255 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Elton.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.senac.tads.pi3a.model;
+
+import java.util.Date;
+import br.senac.tads.pi3a.annotation.Columm;
+import br.senac.tads.pi3a.annotation.Table;
+
+/**
+ *
+ * @author Elton
+ */
+@Table(name = "funcionario")
+public class Funcionario {
+    @Columm(name = "id")
+    private int id;
+    
+    @Columm(name = "status")
+    private boolean status;
+    
+    @Columm(name = "data_admissao")
+    private Date dataAdmissao;
+    
+    @Columm(name = "cargo")
+    private String cargo;
+    
+    @Columm(name = "nome")
+    private String nome;
+    
+    @Columm(name = "cpf")
+    private String cpf;
+    
+    @Columm(name = "data_nasc")
+    private Date dataNascimento;
+    
+    @Columm(name = "sexo")
+    private char sexo;
+    
+    @Columm(name = "est_civil")
+    private String estCivil;
+    
+    @Columm(name = "celular")
+    private String celular;
+    
+    @Columm(name = "telefone")
+    private String telefone;
+    
+    @Columm(name = "email")
+    private String email;
+    
+    @Columm(name = "logradouro")
+    private String logradouro;
+    
+    @Columm(name = "numero")
+    private String numero;
+    
+    @Columm(name = "complemento")
+    private String complemento;
+    
+    @Columm(name = "bairro")
+    private String bairro;
+    
+    @Columm(name = "cep")
+    private String cep;
+    
+    @Columm(name = "cidade")
+    private String cidade;
+    
+    @Columm(name = "uf")
+    private String uf;
+    
+    @Columm(name = "loja_id")
+    private int loja;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(Date dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstCivil() {
+        return estCivil;
+    }
+
+    public void setEstCivil(String estCivil) {
+        this.estCivil = estCivil;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public int getLoja() {
+        return loja;
+    }
+
+    public void setLoja(int loja) {
+        this.loja = loja;
+    }
+}
