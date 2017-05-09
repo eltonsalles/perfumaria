24946006b4bc9b3ab7f406276610b52e299c5c9a
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-05-09 17:21:31 -0300

    Criado uma classe abstract para as classes de validação dos formulários e criado a classe de validação do formulário cliente.

---
+++ perfumaria/src/main/java/br/senac/tads/pi3a/inputFilter/InputFilter.java
@@ -0,0 +1,82 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Elton.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.senac.tads.pi3a.inputFilter;
+
+import br.senac.tads.pi3a.model.Model;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Elton
+ */
+public abstract class InputFilter {
+    protected Map<String, String[]> allMap;
+    
+    protected Map<String, Object> errorValidation;
+
+    public InputFilter(HttpServletRequest request) {
+        this.allMap = request.getParameterMap();
+        this.errorValidation = initErrorValidation(this.allMap);
+    }
+    
+    private Map<String, Object> initErrorValidation(Map<String, String[]> map) {
+        Map<String, Object> error = new LinkedHashMap<>();
+        
+        for (String key : map.keySet()) {
+            if (key.contains("-")) {
+                key = arrangeName(key);
+            }
+            error.put(key, true);
+        }
+        
+        return error;
+    }
+    
+    private String arrangeName(String name) {
+        String[] parts = name.split("-");
+        String result = "";
+        
+        for (int i = 1; i < parts.length; i++) {
+            char firstLetter = parts[i].charAt(0);
+            parts[i] = String.valueOf(firstLetter).toUpperCase() + parts[i]
+                    .substring(1);
+        }
+        
+        for (String part : parts) {
+            result += part;
+        }
+        
+        return result;
+    }
+    
+    public Map<String, Object> getErrorValidation() {
+        return errorValidation;
+    }
+    
+    public abstract boolean isValid();
+    
+    public abstract Model createModel();
+}
---
+++ perfumaria/src/main/java/br/senac/tads/pi3a/inputFilter/InputFilterCliente.java
@@ -0,0 +1,383 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Elton.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.senac.tads.pi3a.inputFilter;
+
+import br.senac.tads.pi3a.dao.DaoCliente;
+import br.senac.tads.pi3a.model.Cliente;
+import br.senac.tads.pi3a.model.Loja;
+import br.senac.tads.pi3a.model.Model;
+import br.senac.tads.pi3a.validation.ValidationAlfaNumerico;
+import br.senac.tads.pi3a.validation.ValidationBoolean;
+import br.senac.tads.pi3a.validation.ValidationCpf;
+import br.senac.tads.pi3a.validation.ValidationDate;
+import br.senac.tads.pi3a.validation.ValidationEmail;
+import br.senac.tads.pi3a.validation.ValidationString;
+import br.senac.tads.pi3a.validation.ValidationTamanho;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Elton
+ */
+public class InputFilterCliente extends InputFilter {
+    public InputFilterCliente(HttpServletRequest request) {
+        super(request);
+    }
+    
+    /**
+     * Faz a validação do formulário cliente
+     * 
+     * @return 
+     */
+    @Override
+    public boolean isValid() {
+        boolean result = false;
+        
+        ValidationTamanho validationTamanho = new ValidationTamanho();
+        ValidationAlfaNumerico validationAlfaNumerico
+                = new ValidationAlfaNumerico();
+        
+        // Verifica o nome do formulário cliente validando o tamanho e 
+        // deixando apenas caracteres válidos na string
+        if (this.allMap.containsKey("nome")) {
+            ValidationString validationString = new ValidationString();
+            
+            validationTamanho.setTamanho(150);
+            
+            if (validationTamanho.isValid(this.allMap.get("nome")[0]) &&
+                    validationString.isValid(this.allMap.get("nome")[0])) {
+                this.errorValidation.replace("nome", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica se a data de nascimento informada no formulário cliente
+        // é uma data válida
+        if (this.allMap.containsKey("data-nascimento")) {
+            ValidationDate validationDate = new ValidationDate();
+            
+            if (validationDate.isValid(this.allMap.get("data-nascimento")[0])) {
+                this.errorValidation.replace("dataNascimento", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica se o cpf do formulário cliente informado é válido e se
+        // ele ainda não existe na base de dados
+        if (this.allMap.containsKey("cpf")) {
+            // Deixa só os digitos
+            String cpf = this.allMap.get("cpf")[0].replaceAll("\\D", "");
+            
+            ValidationCpf validationCpf = new ValidationCpf();
+            
+            // Se for um cpf válido olha no banco para verificar que não existe
+            if (validationCpf.isValid(cpf)) {
+                DaoCliente daoCliente = new DaoCliente();
+                
+                try {
+                    List lista = daoCliente.findAll(new Cliente(), "cpf", "=",
+                            cpf);
+                    
+                    if (lista.isEmpty()) {
+                        this.errorValidation.replace("cpf", false);
+                        this.allMap.replace("cpf", new String[]{cpf});
+                        result = true;
+                    } else {
+                        result = false;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace(System.err);
+                    result = false;
+                }
+            }
+        }
+        
+        // Verifica se o valor de status do formulário cliente é um boolean
+        if (this.allMap.containsKey("status")) {
+            ValidationBoolean validationBoolean = new ValidationBoolean();
+            
+            if (validationBoolean.isValid(this.allMap.get("status")[0])) {
+                this.errorValidation.replace("status", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica o email do formulário cliente e garante que email seja
+        // em um padrão válido
+        if (this.allMap.containsKey("email")) {
+            ValidationEmail validationEmail = new ValidationEmail();
+            
+            validationTamanho.setTamanho(150);
+            
+            if (validationTamanho.isValid(this.allMap.get("email")[0])
+                    && validationEmail.isValid(this.allMap.get("email")[0])) {
+                this.errorValidation.replace("email", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Garante que algo esteja selecionado no campo estado civil
+        // do formulário cliente
+        if (this.allMap.containsKey("estado-civil")) {
+            String estadoCivil = this.allMap.get("estado-civil")[0];
+            
+            if (!estadoCivil.isEmpty()) {
+                if (estadoCivil.equalsIgnoreCase("solteiro")
+                        || estadoCivil.equalsIgnoreCase("casado")
+                        || estadoCivil.equalsIgnoreCase("divorciado")
+                        || estadoCivil.equalsIgnoreCase("viuvo")) {
+                    this.errorValidation.replace("estadoCivil", false);
+                    result = true;
+                } else {
+                    result = false;
+                }
+            } else {
+                result = false;
+            }
+        }
+        
+        // Garante que o valor do campo sexo do formulário cliente
+        // seja F ou M
+        if (this.allMap.containsKey("sexo")) {
+            if (this.allMap.get("sexo")[0].equalsIgnoreCase("F")
+                    || this.allMap.get("sexo")[0].equalsIgnoreCase("M")) {
+                this.errorValidation.replace("sexo", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica o valor e o tamanho de celular no formulário cliente
+        if (this.allMap.containsKey("celular")) {
+            // Deixa apenas digitos
+            String celular = this.allMap.get("celular")[0]
+                    .replaceAll("\\D", "");
+            
+            validationTamanho.setTamanho(11);
+            
+            if (validationTamanho.isValid(celular)) {
+                this.errorValidation.replace("celular", false);
+                this.allMap.replace("celular", new String[]{celular});
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica o valor e o tamanho de telefone no formulário cliente
+        if (this.allMap.containsKey("telefone")) {
+            // Deixa apenas digitos
+            String telefone = this.allMap.get("telefone")[0]
+                    .replaceAll("\\D", "");
+            
+            validationTamanho.setTamanho(11);
+            
+            if (validationTamanho.isValid(telefone)) {
+                this.errorValidation.replace("telefone", false);
+                this.allMap.replace("telefone", new String[]{telefone});
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica o logradouro do formulário cliente validando o tamanho e 
+        // deixando apenas caracteres válidos na string
+        if (this.allMap.containsKey("logradouro")) {
+            validationTamanho.setTamanho(150);
+            
+            if (validationTamanho.isValid(this.allMap.get("logradouro")[0]) &&
+                    validationAlfaNumerico.isValid(this.allMap
+                            .get("logradouro")[0])) {
+                this.errorValidation.replace("logradouro", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica o número (endereço) do formulário cliente validando o 
+        // tamanho e deixando apenas caracteres válidos na string
+        if (this.allMap.containsKey("numero")) {
+            validationTamanho.setTamanho(10);
+            
+            if (validationTamanho.isValid(this.allMap.get("numero")[0]) &&
+                    validationAlfaNumerico.isValid(this.allMap
+                            .get("numero")[0])) {
+                this.errorValidation.replace("numero", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // O campo complemento do formulário de cliente não é obrigatório
+        // então apenas verifica se houver algum valor diferente de ""
+        if (this.allMap.containsKey("complemento")) {
+            if (!this.allMap.get("complemento")[0].isEmpty()) {
+                validationTamanho.setTamanho(50);
+                
+                if (validationTamanho.isValid(this.allMap.get("complemento")[0])
+                        && validationAlfaNumerico.isValid(this.allMap
+                                .get("complemento")[0])) {
+                    this.errorValidation.replace("complemento", false);
+                    result = true;
+                } else {
+                    result = true;
+                }
+            } else {
+                this.errorValidation.replace("complemento", false);
+                result = true;
+            }
+        }
+        
+        // Verifica o bairro do formulário cliente validando o tamanho e 
+        // deixando apenas caracteres válidos na string
+        if (this.allMap.containsKey("bairro")) {
+            validationTamanho.setTamanho(50);
+            
+            if (validationTamanho.isValid(this.allMap.get("bairro")[0]) &&
+                    validationAlfaNumerico.isValid(this.allMap
+                            .get("bairro")[0])) {
+                this.errorValidation.replace("bairro", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica a cidade do formulário cliente validando o tamanho e 
+        // deixando apenas caracteres válidos na string
+        if (this.allMap.containsKey("cidade")) {
+            validationTamanho.setTamanho(50);
+            
+            if (validationTamanho.isValid(this.allMap.get("cidade")[0]) &&
+                    validationAlfaNumerico.isValid(this.allMap
+                            .get("cidade")[0])) {
+                this.errorValidation.replace("cidade", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica se o valor do campo uf do formulário cliente é igual
+        // a algum da lista
+        if (this.allMap.containsKey("uf")) {
+            List ufs = new ArrayList();
+            ufs.addAll(Arrays.asList(new String[]{"AC", "AL", "AP", "AM", "BA",
+                "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PR", "PB",
+                "PA", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SE",
+                "SP", "TO"}));
+            
+            if (ufs.contains(this.allMap.get("uf")[0])) {
+                this.errorValidation.replace("uf", false);
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        // Verifica o valor e o tamanho de cep no formulário cliente
+        if (this.allMap.containsKey("cep")) {
+            // Deixa apenas digitos
+            String cep = this.allMap.get("cep")[0].replaceAll("\\D", "");
+            
+            validationTamanho.setTamanho(8);
+            
+            if (validationTamanho.isValid(cep)) {
+                this.errorValidation.replace("cep", false);
+                this.allMap.replace("cep", new String[]{cep});
+                result = true;
+            } else {
+                result = false;
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * Retorna um objeto cliente com todos os dados preenchidos conforme as
+     * informações validadas
+     * 
+     * @return 
+     */
+    @Override
+    public Model createModel() {
+        Cliente cliente = new Cliente();
+        
+        try {
+            cliente.setStatus(Boolean.valueOf(this.allMap.get("status")[0]));
+            cliente.setCpf(this.allMap.get("cpf")[0]);
+            cliente.setNome(this.allMap.get("nome")[0]);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date dataNascimento = new Date(sdf.parse(this.allMap
+                    .get("data-nascimento")[0]).getTime());
+            cliente.setDataNascimento(dataNascimento);
+            
+            cliente.setSexo(this.allMap.get("sexo")[0].charAt(0));
+            cliente.setEstadoCivil(this.allMap.get("estado-civil")[0]);
+            cliente.setCelular(this.allMap.get("celular")[0]);
+            cliente.setTelefone(this.allMap.get("telefone")[0]);
+            cliente.setEmail(this.allMap.get("email")[0]);
+            cliente.setLogradouro(this.allMap.get("logradouro")[0]);
+            cliente.setNumero(this.allMap.get("numero")[0]);
+            
+            if (!this.allMap.get("complemento")[0].isEmpty()) {
+                cliente.setComplemento(this.allMap.get("complemento")[0]);
+            }
+            
+            cliente.setBairro(this.allMap.get("bairro")[0]);
+            cliente.setCep(this.allMap.get("cep")[0]);
+            cliente.setCidade(this.allMap.get("cidade")[0]);
+            cliente.setUf(this.allMap.get("uf")[0]);
+
+            // #MOCK
+            Loja loja = new Loja();
+            loja.setId(1);
+            cliente.setLoja(loja);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            cliente = null;
+        }
+        
+        return cliente;
+    }
+}
