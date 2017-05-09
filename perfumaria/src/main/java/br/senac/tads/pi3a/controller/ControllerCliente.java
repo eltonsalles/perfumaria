23326875c358cd59f9c84054ad6dc80569cffee5
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-05-09 17:23:30 -0300

    Arrumando a classe ControllerCliente para fazer a validação dos dados do formulário de cliente no servidor.

--- perfumaria/src/main/java/br/senac/tads/pi3a/controller/ControllerCliente.java
+++ perfumaria/src/main/java/br/senac/tads/pi3a/controller/ControllerCliente.java
@@ -24,6 +24,7 @@
 package br.senac.tads.pi3a.controller;
 
 import br.senac.tads.pi3a.dao.DaoCliente;
+import br.senac.tads.pi3a.inputFilter.InputFilterCliente;
 import br.senac.tads.pi3a.model.Cliente;
 import br.senac.tads.pi3a.model.Loja;
 import br.senac.tads.pi3a.model.Model;
@@ -40,48 +41,39 @@ import javax.servlet.http.HttpSession;
  */
 public class ControllerCliente implements Logica {
     @Override
-    public String novo(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
+    public String novo(HttpServletRequest request, HttpServletResponse response,
+            HttpSession session) throws Exception {
         try {
             // Se o formulário for submetido por post então entra aqui
             if (request.getMethod().equalsIgnoreCase("post")) {
-                // Implatar validação...
+                // Classe de validação do formulário cliente
+                InputFilterCliente inputFilterCliente
+                        = new InputFilterCliente(request);
+                
+                // Faz a validação do formulário cliente
+                if (inputFilterCliente.isValid()) {
+                    // Estando tudo certo, então é criado o objeto cliente com
+                    // os dados do formulário
+                    Cliente cliente = (Cliente) inputFilterCliente
+                            .createModel();
+                    
+                    DaoCliente dao = new DaoCliente(cliente);
 
-                /**
-                 * #Mock - Pegando os dados do formulário e apenas arrumando
-                 * o tamanho para fazer a inserção
-                 */
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-                Cliente cliente = new Cliente();
-                cliente.setStatus(Boolean.valueOf(request.getParameter("status")));
-                cliente.setCpf(request.getParameter("cpf").replaceAll("\\D", ""));
-                cliente.setNome(request.getParameter("nome"));
-
-                Date dataNascimento = new Date(sdf.parse(request.getParameter("data-nascimento")).getTime());
-                cliente.setDataNascimento(dataNascimento);
-                cliente.setSexo(request.getParameter("sexo").charAt(0));
-                cliente.setEstadoCivil(request.getParameter("estado-civil"));
-                cliente.setCelular(request.getParameter("celular").replaceAll("\\D", ""));
-                cliente.setTelefone(request.getParameter("telefone").replaceAll("\\D", ""));
-                cliente.setEmail(request.getParameter("email"));
-                cliente.setLogradouro(request.getParameter("logradouro"));
-                cliente.setNumero(request.getParameter("numero"));
-                cliente.setComplemento(request.getParameter("complemento"));
-                cliente.setBairro(request.getParameter("bairro"));
-                cliente.setCep(request.getParameter("cep").replaceAll("\\D", ""));
-                cliente.setCidade(request.getParameter("cidade"));
-                cliente.setUf(request.getParameter("uf"));
-
-                Loja loja = new Loja();
-                loja.setId(1);
-                cliente.setLoja(loja);
-
-                DaoCliente dao = new DaoCliente(cliente);
-
-                if (dao.insert() != -1) {
-                    session.setAttribute("alert", "alert-success");
-                    session.setAttribute("alertMessage", "Cadastro realizado com sucesso.");
-                    return "novo";
+                    // A dao retorna um id válido se conseguir fazer a inserção
+                    if (dao.insert() != -1) {
+                        session.setAttribute("alert", "alert-success");
+                        session.setAttribute("alertMessage",
+                                "Cadastro realizado com sucesso.");
+                        return "novo";
+                    }
+                } else {
+                    // Manda para a jsp os campos inválidos e uma mensagem
+                    // de aviso
+                    session.setAttribute("errorValidation",
+                            inputFilterCliente.getErrorValidation());
+                    session.setAttribute("alert", "alert-danger");
+                    session.setAttribute("alertMessage",
+                            "Verifique os campo em vermelho.");
                 }
             }
             
@@ -89,7 +81,8 @@ public class ControllerCliente implements Logica {
         } catch (Exception e) {
             e.printStackTrace(System.err);
             session.setAttribute("alert", "alert-danger");
-            session.setAttribute("alertMessage", "Não foi possível realizar o cadastro.");
+            session.setAttribute("alertMessage",
+                    "Não foi possível realizar o cadastro.");
             return "novo";
         }
     }
