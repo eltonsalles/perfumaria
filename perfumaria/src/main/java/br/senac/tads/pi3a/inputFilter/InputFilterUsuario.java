d27a79ebf2f04cb6d9a5c84f8b12a8db66a56832
joaohamasaki <hamasaki.joao@gmail.com>
2017-05-12 22:18:46 -0300

    Verificação dos campos de usuário

--- perfumaria/src/main/java/br/senac/tads/pi3a/inputFilter/InputFilterUsuario.java
+++ perfumaria/src/main/java/br/senac/tads/pi3a/inputFilter/InputFilterUsuario.java
@@ -24,6 +24,10 @@
 package br.senac.tads.pi3a.inputFilter;
 
 import br.senac.tads.pi3a.model.Model;
+import br.senac.tads.pi3a.model.Usuario;
+import br.senac.tads.pi3a.validation.ValidationAlpha;
+import br.senac.tads.pi3a.validation.ValidationInt;
+import br.senac.tads.pi3a.validation.ValidationTamanho;
 import java.util.Map;
 
 /**
@@ -38,12 +42,75 @@ public class InputFilterUsuario extends InputFilter {
 
     @Override
     public boolean isValid() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        ValidationTamanho validationTamanho = new ValidationTamanho();
+        // Garante que o id do formulário usuário está vazio ou que é um inteiro
+        // maior que 0
+        if (this.allMap.containsKey("id")) {
+            if (!this.allMap.get("id")[0].isEmpty()) {
+                ValidationInt validationInt = new ValidationInt();
 
-    @Override
-    protected Model getModel() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                if (validationInt.isValid(this.allMap.get("id")[0])) {
+                    if (Integer.valueOf(this.allMap.get("id")[0]) > 0) {
+                        this.errorValidation.replace("id", false);
+                    }
+                } else {
+                    this.errorValidation.replace("id", false);
+                }
+            }
+        }
+        // Verifica o nome do formulário usuario(login) validando o tamanho e 
+        // deixando apenas caracteres válidos na string
+        if (this.allMap.containsKey("login")) {
+            ValidationAlpha validationAlpha = new ValidationAlpha();
+            
+            validationTamanho.setTamanho(50);
+            
+            
+           if (validationTamanho.isValid(this.allMap.get("login")[0])
+                    && validationAlpha.isValid(this.allMap.get("login")[0])) {
+                this.errorValidation.replace("login", false);
+            }  
+        }
+        // Verifica o nome do formulário usuario(senha) validando o tamanho e 
+        // deixando apenas caracteres válidos na string
+        if (this.allMap.containsKey("senha")) {
+            ValidationAlpha validationAlpha = new ValidationAlpha();
+            
+            validationTamanho.setTamanho(8);
+            
+            if (validationTamanho.isValid(this.allMap.get("senha")[0])
+                    && validationAlpha.isValid(this.allMap.get("senha")[0])) {
+                this.errorValidation.replace("senha", false);
+            }  
+        }
+        //Verifica se algum campo está selecionado no campo Nível de Usuário
+        //do formulário Usuario
+        if (this.allMap.containsKey("nivel-usuario")) {
+            String nivelUsuario = this.allMap.get("nivel-usuario")[0];
+            
+            if (!nivelUsuario.isEmpty()) {
+                if (nivelUsuario.equalsIgnoreCase("1")
+                        || nivelUsuario.equalsIgnoreCase("2")
+                        || nivelUsuario.equalsIgnoreCase("3")
+                        || nivelUsuario.equalsIgnoreCase("4")
+                        || nivelUsuario.equalsIgnoreCase("5")
+                        || nivelUsuario.equalsIgnoreCase("6")
+                        || nivelUsuario.equalsIgnoreCase("7")
+                        || nivelUsuario.equalsIgnoreCase("8")) {
+                    this.errorValidation.replace("nivelUsuario", false);
+                }
+            }
+        }
+          return this.errorStatus();
     }
-    
+        @Override
+        protected Model getModel(){
+            Usuario usuario = new Usuario();
+            
+            
+            {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        }
 }
