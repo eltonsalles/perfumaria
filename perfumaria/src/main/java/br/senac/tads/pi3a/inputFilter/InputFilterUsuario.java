212ba04ca5d698347686f012802fee7486cb9651
joaohamasaki <hamasaki.joao@gmail.com>
2017-05-13 10:38:55 -0300

    Finalizado o getModel.

--- perfumaria/src/main/java/br/senac/tads/pi3a/inputFilter/InputFilterUsuario.java
+++ perfumaria/src/main/java/br/senac/tads/pi3a/inputFilter/InputFilterUsuario.java
@@ -23,7 +23,9 @@
  */
 package br.senac.tads.pi3a.inputFilter;
 
+import br.senac.tads.pi3a.model.Funcionario;
 import br.senac.tads.pi3a.model.Model;
+import br.senac.tads.pi3a.model.NivelUsuario;
 import br.senac.tads.pi3a.model.Usuario;
 import br.senac.tads.pi3a.validation.ValidationAlpha;
 import br.senac.tads.pi3a.validation.ValidationInt;
@@ -58,59 +60,91 @@ public class InputFilterUsuario extends InputFilter {
                 }
             }
         }
+        // Garante que o id do formulário funcionario está vazio ou que é um inteiro
+        // maior que 0
+        if (this.allMap.containsKey("funcionario")) {
+            if (!this.allMap.get("funcionario")[0].isEmpty()) {
+                ValidationInt validationInt = new ValidationInt();
+
+                if (validationInt.isValid(this.allMap.get("funcionario")[0])) {
+                    if (Integer.valueOf(this.allMap.get("funcionario")[0]) > 0) {
+                        this.errorValidation.replace("funcionario", false);
+                    } else {
+                        this.errorValidation.replace("funcionario", false);
+                    }
+                }
+            }
+        }
         // Verifica o nome do formulário usuario(login) validando o tamanho e 
         // deixando apenas caracteres válidos na string
         if (this.allMap.containsKey("login")) {
             ValidationAlpha validationAlpha = new ValidationAlpha();
-            
+
             validationTamanho.setTamanho(50);
-            
-            
-           if (validationTamanho.isValid(this.allMap.get("login")[0])
+
+            if (validationTamanho.isValid(this.allMap.get("login")[0])
                     && validationAlpha.isValid(this.allMap.get("login")[0])) {
                 this.errorValidation.replace("login", false);
-            }  
+            }
         }
         // Verifica o nome do formulário usuario(senha) validando o tamanho e 
         // deixando apenas caracteres válidos na string
         if (this.allMap.containsKey("senha")) {
             ValidationAlpha validationAlpha = new ValidationAlpha();
-            
+
             validationTamanho.setTamanho(8);
-            
+
             if (validationTamanho.isValid(this.allMap.get("senha")[0])
                     && validationAlpha.isValid(this.allMap.get("senha")[0])) {
                 this.errorValidation.replace("senha", false);
-            }  
+            }
         }
         //Verifica se algum campo está selecionado no campo Nível de Usuário
         //do formulário Usuario
         if (this.allMap.containsKey("nivel-usuario")) {
             String nivelUsuario = this.allMap.get("nivel-usuario")[0];
-            
+
             if (!nivelUsuario.isEmpty()) {
-                if (nivelUsuario.equalsIgnoreCase("1")
-                        || nivelUsuario.equalsIgnoreCase("2")
-                        || nivelUsuario.equalsIgnoreCase("3")
-                        || nivelUsuario.equalsIgnoreCase("4")
-                        || nivelUsuario.equalsIgnoreCase("5")
-                        || nivelUsuario.equalsIgnoreCase("6")
-                        || nivelUsuario.equalsIgnoreCase("7")
-                        || nivelUsuario.equalsIgnoreCase("8")) {
+                if (nivelUsuario.equals("1")
+                        || nivelUsuario.equals("2")
+                        || nivelUsuario.equals("3")
+                        || nivelUsuario.equals("4")
+                        || nivelUsuario.equals("5")
+                        || nivelUsuario.equals("6")
+                        || nivelUsuario.equals("7")
+                        || nivelUsuario.equals("8")) {
                     this.errorValidation.replace("nivelUsuario", false);
                 }
             }
         }
-          return this.errorStatus();
+        return this.errorStatus();
     }
-        @Override
-        protected Model getModel(){
-            Usuario usuario = new Usuario();
-            
-            
-            {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    @Override
+    protected Model getModel() {
+        Usuario usuario = new Usuario();
+
+        try {
+            if (!this.allMap.get("id")[0].isEmpty()) {
+                usuario.setId(Integer.valueOf(this.allMap.get("id")[0]));
+            }
+            Funcionario funcionario = new Funcionario();
+            funcionario.setId(Integer.valueOf(this.allMap.get("funcionario")[0]));
+            usuario.setFuncionario(funcionario);
+
+            NivelUsuario nivelUsuario = new NivelUsuario();
+            nivelUsuario.setId(Integer.valueOf(this.allMap.get("nivel-usuario")[0]));
+            usuario.setNivelUsuario(nivelUsuario);
+
+            usuario.setNome(this.allMap.get("nome")[0]);
+            usuario.setSenha(this.allMap.get("senha")[0]);
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace(System.err);
+            usuario = null;
         }
 
-        }
+        return usuario;
+    }
+
 }
