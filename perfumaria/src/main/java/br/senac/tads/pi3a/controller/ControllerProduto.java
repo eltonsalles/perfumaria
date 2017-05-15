13352b88326b4a68c87531688b269f4eaa2e2023
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-05-14 21:18:19 -0300

    Refatorado o método novo da classe ControllerProduto para fazer a inserção no banco corretamente.

--- perfumaria/src/main/java/br/senac/tads/pi3a/controller/ControllerProduto.java
+++ perfumaria/src/main/java/br/senac/tads/pi3a/controller/ControllerProduto.java
@@ -27,11 +27,9 @@ import br.senac.tads.pi3a.dao.DaoItensLoja;
 import br.senac.tads.pi3a.dao.DaoProduto;
 import br.senac.tads.pi3a.inputFilter.InputFilterProduto;
 import br.senac.tads.pi3a.model.ItensLoja;
-import br.senac.tads.pi3a.model.Model;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 /**
  *
@@ -40,161 +38,100 @@ import java.util.List;
 public class ControllerProduto implements Logica {
 
     @Override
-    public String novo(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
-       
-        try{
-            if(request.getMethod().equalsIgnoreCase("post")){
-                    
-                InputFilterProduto inputFilterProduto = new InputFilterProduto(request.getParameterMap());
-                
-                // cria objeto itens de loja
-                
-                ItensLoja itensLoja =  (ItensLoja)inputFilterProduto.getData();
-                
-                if(inputFilterProduto.isValid()){
-                    
+    public String novo(HttpServletRequest request, HttpServletResponse response,
+            HttpSession session) throws Exception {
+        try {
+            // Se o formulário for submetido por post então entra aqui
+            if (request.getMethod().equalsIgnoreCase("post")) {
+                // Classe de validação do formulário cliente
+                InputFilterProduto inputFilterProduto
+                        = new InputFilterProduto(request.getParameterMap());
+
+                // Cria um objeto itens loja com os dados do formulário,
+                // mas sem validação
+                ItensLoja itensLoja = (ItensLoja) inputFilterProduto.getData();
+
+                // Faz a validação do formulário produto
+                if (inputFilterProduto.isValid()) {
+                    // Atualiza o objeto itens loja com os dados validados
                     itensLoja = (ItensLoja) inputFilterProduto.createModel();
-                    
-                    DaoProduto dao = new DaoProduto();
-                    
-                    if(dao.findAll(itensLoja,"nome","=",itensLoja.getProduto().getNome())
-                            .isEmpty()){
-                        
-                        itensLoja.setStatus(true);
-                        
-                        
-                        if(dao.insert() !=-1){
-                            session.setAttribute("alert", "alert-sucess");
-                            session.setAttribute("alertMessage","Cadastro realizado com sucesso" );
-                            return "novo";
+
+                    DaoProduto daoProduto = new DaoProduto(itensLoja
+                            .getProduto());
+
+                    // Garante que o nome não esteja cadastrado na base de dados
+                    if (daoProduto.findAll(itensLoja.getProduto(), "nome", "=",
+                            itensLoja.getProduto().getNome()).isEmpty()) {
+                        // Insere a parte dos dados gerais do produto
+                        int idProduto = daoProduto.insert();
+
+                        if (idProduto != -1) {
+                            // Id gerado ao inserir os dados gerais de produto
+                            itensLoja.getProduto().setId(idProduto);
+
+                            // Todo item deve ser cadastrado com status true
+                            itensLoja.setStatus(true);
+
+                            DaoItensLoja daoItensLoja
+                                    = new DaoItensLoja(itensLoja);
+
+                            if (daoItensLoja.insert() != -1) {
+                                session.setAttribute("alert", "alert-success");
+                                session.setAttribute("alertMessage",
+                                        "Cadastro realizado com sucesso");
+                                return "novo";
+                            }
                         }
-                    }else{
+                    } else {
                         session.setAttribute("itensLoja", itensLoja);
                         session.setAttribute("alert", "alert-danger");
-                        session.setAttribute("alertMessage", "Produto já cadastrado.");
-                        
+                        session.setAttribute("alertMessage",
+                                "Este nome de produto já está cadastrado.");
                     }
-                }else{
-                    
-                     session.setAttribute("itensLoja", itensLoja);
-                        session.setAttribute("alert", "alert-danger");
-                        session.setAttribute("alertMessage", "Verifique os campos em vermelho.");
-                    
+                } else {
+                    // Manda para a jsp os campos inválidos e uma mensagem
+                    session.setAttribute("errorValidation",
+                            inputFilterProduto.getErrorValidation());
+                    session.setAttribute("itensLoja", itensLoja);
+                    session.setAttribute("alert", "alert-danger");
+                    session.setAttribute("alertMessage",
+                            "Verifique os campos em vermelho.");
                 }
             }
-            
+
             return "WEB-INF/jsp/cadastrar-produto.jsp";
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace(System.err);
             session.setAttribute("alert", "alert-danger");
-            session.setAttribute("alertMessage", "Não foi possivel realizar o cadastro.");
+            session.setAttribute("alertMessage",
+                    "Não foi possivel realizar o cadastro.");
             return "novo";
         }
-        
-        
-        
     }
 
     @Override
     public String editar(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
-       try{
-           if(request.getMethod().equalsIgnoreCase("post")) {
-               InputFilterProduto inputFilterProduto = new InputFilterProduto(request.getParameterMap());
-          
-           ItensLoja itensLoja = new ItensLoja();
-           
-           if(inputFilterProduto.isValid()){
-               
-               itensLoja = (ItensLoja) inputFilterProduto.createModel();
-               
-               DaoProduto dao = new DaoProduto(itensLoja.getProduto());
-               DaoItensLoja daoLoja = new DaoItensLoja(itensLoja);
-               
-               List<Model> lista = dao.findAll(itensLoja,"nome","=",
-                       itensLoja.getProduto().getNome());
-              
-               if(lista.size() == 1){
-                   if(lista.get(0).getId() == itensLoja.getId()){
-                       if(dao.update()) {
-                           session.setAttribute("alert", "alert-success");
-                                session.setAttribute("alertMessage",
-                                        "Cadastro alterado com sucesso.");
-                                session.setAttribute("id", itensLoja.getId());
-                                return "editar";
-                        }
-                   }else{
-                        session.setAttribute("cliente", itensLoja);
-                            session.setAttribute("alert", "alert-danger");
-                            session.setAttribute("alertMessage",
-                                    "Este produto já está cadastrado.");
-                        
-                   }
-               }else{
-                   session.setAttribute("cliente", itensLoja);
-                        session.setAttribute("alert", "alert-danger");
-                        session.setAttribute("alertMessage",
-                                "Não foi encontrado nenhum produto com o nome"
-                                        + " informado.");
-                   
-                   
-               }
-           }else{
-               session.setAttribute("errorValidation",
-                            inputFilterProduto.getErrorValidation());
-                    session.setAttribute("cliente", itensLoja);
-                    session.setAttribute("alert", "alert-danger");
-                    session.setAttribute("alertMessage",
-                            "Verifique os campo em vermelho.");
-           }
-           
-           
-           }
-           
-          if (request.getParameter("id") != null) {
-                String id = request.getParameter("id");
-                boolean digito = true;
-
-                for (int i = 0; i < id.length(); i++) {
-                    if (!Character.isDigit(id.charAt(i))) {
-                        digito = false;
-                        break;
-                    }
-                }
-              if (digito) {
-                    Model itensLoja = new ItensLoja();
-                    DaoProduto dao = new DaoProduto();
-                    itensLoja = dao.findOne(itensLoja, Integer.valueOf(request
-                            .getParameter("id")));
-
-                    session.setAttribute("itensLoja", itensLoja);
-                }
-            }
-
-       
-       }catch(Exception e){
-           
-       }
-       return "/WEB-INF/jsp/cadastrar-produto.jsp";
+        return "/WEB-INF/jsp/cadastrar-produto.jsp";
     }
 
     @Override
     public String excluir(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "/WEB-INF/jsp/cadastrar-produto.jsp";
     }
 
     @Override
     public String pesquisar(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
         return "/WEB-INF/jsp/consultar-produto.jsp";
     }
-    
+
     public String movimentar(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         return "/WEB-INF/jsp/manutencao-produto.jsp";
     }
-    
+
     public String relatorio(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         return "/WEB-INF/jsp/relatorio-estoque.jsp";
     }
-    
+
     public String historico(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         return "/WEB-INF/jsp/historico-produto.jsp";
     }
